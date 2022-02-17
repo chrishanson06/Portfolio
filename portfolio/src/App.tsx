aa0c6b177c89abeb0a4aa32e7e5688d829dee676
Dennis James Stelmach <dj@dstelmach.com>
2022-02-17 14:26:24 -0500

    fix logo

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
