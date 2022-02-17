56eeb11c2355c06969114ddff123d3ff46db803f
chrishanson06 <chrishanson06@gmail.com>
2022-02-17 08:01:58 -0500

    Header, Footer, Logo
    Created a header and footer for the page, and created a logo in a svg. Logo was purchased for $2

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import <logo className="svg"></logo> ;
+import "./App.css";
+import Header from "./componets/Header/Header";
+import Footer from "./componets/Footer/Footer";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./pages/Home/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<BrowserRouter>
+			<Routes>
+				<Route index element={<Home />} />
+
+
+			</Routes>
+			</BrowserRouter>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
